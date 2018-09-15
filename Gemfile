0253ed2af2288c1eb780c051572f8504a182c3a1
Katsuma Narisawa <face.the.music721@gmail.com>
2018-09-15 23:08:06 +0900

    Initialize

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ruby '2.5.1'
+
+gem 'activerecord'
+gem 'mysql2'
+gem 'nokogiri'
+gem 'pry-byebug'
+gem 'rake'
+gem 'slack-notifier'
+gem 'selenium-webdriver'
---
+++ config/initializers.rb
@@ -0,0 +1,15 @@
+require 'active_record'
+require 'erb'
+require 'nokogiri'
+require 'pry-byebug'
+require 'selenium-webdriver'
+require 'slack-notifier'
+require './app/models/application_record'
+require './app/models/user'
+
+# setup active record
+config = YAML.safe_load(ERB.new(IO.read(File.expand_path('database.yml', __dir__))).result)
+env = ENV.fetch('APP_ENV', 'development')
+ActiveRecord::Base.establish_connection(config[env])
+ActiveRecord::Base.time_zone_aware_attributes = true
+Time.zone = 'Tokyo'
